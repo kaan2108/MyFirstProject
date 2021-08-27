c0a0d9508e265a7a8600f884584d399a967450f2
kaan2108 <yuecel1@tritons.iowacentral.edu>
2021-08-26 23:08:27 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,30 @@
+/*
+	Kaan Yuecel
+	C++ Fall 2021
+	Due: 09/01/2021
+	Lab 1, Introduction to C++ Programming
+	This lab will serve as a demonstration of your ability to download a project from GitHub, 
+	compile and run the project, upload a project to GitHub, and submit a project to Google Classroom. 
+	This lab assumes you have downloaded and installed an IDE (integrated development environment) 
+	and have also installed GitHub Desktop. If you have not met the assumptions for the lab, the steps 
+	you need to complete the lab may vary from what is here. The goals are for you to download
+	a project, compile and run that project, and to upload your own project. If you meet the goals, 
+	you will get the points for this lab. Follow the directions in this lab and complete the tasks. 
+	If you have questions, please ask.
+
+
+*/
+
+// I am happy :)
+
+#include <iostream>		
+
+
+int main()
+{
+	
+	std::cout << "Hello World! I love red";
+
+
+	return 0;
+}
